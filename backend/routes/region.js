5eca7725c2168d3299a591ce9f8c23dc2851c5df
siddharth shah <siddharthshah3030@gmail.com>
2019-01-14 07:33:21 +0530

    cleaning and api ready

--- backend/routes/region.js
+++ backend/routes/region.js
@@ -7,35 +7,9 @@ var mongoose = require('mongoose');
 /* GET home page. */
 router.get('/:name', function(req, res, next) {
     var name = req.params.name;
-    // console.log(name)
     region.findOne({ name: name }).exec(function (err, doc) {
-
-
-        
-        
-        
-        // str = JSON.stringify(doc);
-        // str = JSON.stringify(doc, null, 4); // (Optional) beautiful indented output.
-        // console.log(str);  
-        res.render('index', { title: doc });
-        // console.log("in location here");  
-        // id = '5c3bdd9386bad829eea774db'
-        // region.findOne({ name: name }).exec(function (err, he) {
-
-        //     str = JSON.stringify(he);
-        //     str = JSON.stringify(he, null, 4); // (Optional) beautiful indented output.
-        //     console.log(str);        });
-    // });
-
-
+        res.json( doc );
     });
-
-    // };
-    // promise(function (err, docs) {
-
-
-    // });
-
 });
 
 
