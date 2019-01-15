22b5139af434f8f297bb75541f2c68721cd55e2d
siddharth shah <siddharthshah3030@gmail.com>
2019-01-16 04:42:37 +0530

    finalizing block chain party instance

--- data/seedBlockchain/partyBlockchain.js
+++ data/seedBlockchain/partyBlockchain.js
@@ -1,26 +1,10 @@
-// var mongoose = require('mongoose');
-// var parties = require('../schema/party'); 
 
-
-var ij = 1;
 party.find({}, function(err, currentParty) {
-    // console.log("inside findx of parties for block chain")
     
     currentParty.forEach(function(partyInstance) {
         // @shritesh do you function here
-        console.log(ij)
-        ij++
+        console.log(partyInstance)
 
     });
 }
 )
-
-// // console.log("hell from block chain party seeding ")
-// var name = "lovation3";
-
-    
-//     console.log( "inside main file");
-//     party.find().exec(function (err, doc) {
-//         console.log( doc );
-//     });
-
