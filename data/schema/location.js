772fb0b91ae272e5a87051a2a7ba2032a5fb921b
siddharth shah <siddharthshah3030@gmail.com>
2019-01-16 05:00:13 +0530

    new campaign schema

--- data/schema/location.js
+++ data/schema/location.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
     name:    String,
+    locationName : String,
+    chairPName : String,
+    ChairPIdentity : String,
+    
     candidates : [ {name : String, party : { name : String ,abb: String, symbol : String}} ]
 })
 
