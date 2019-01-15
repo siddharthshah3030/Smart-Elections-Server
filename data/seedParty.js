1976ad0225644b8a0dee5472cf12817067cc0ab2
siddharth shah <siddharthshah3030@gmail.com>
2019-01-16 04:10:01 +0530

    adding modifiesd party seeding file

--- data/seedParty.js
+++ data/seedParty.js
@@ -2,7 +2,7 @@
 var faker = require('faker')
 
 var mongoose = require('mongoose');
-var db = require('./schema/voter');
+var db = require('./schema/party');
 
 var abbParties = ['BJP','AITC','BSP','CPI','INC', 'NCP','AAP','NPP','SS','SP'];
 var parties = [
@@ -32,7 +32,7 @@ var symbol = [
 ]
 
 
-faker.locale = "en_IND";
+// faker.locale = "en_IND";
 
 function makeid() {
     var text = "";
@@ -46,28 +46,19 @@ function makeid() {
   
   console.log(makeid());
 
-for(var i=111;i<10200;i++){
-    var cnt = i%100;
-    if(i==0){ cntx = 100;}
-    faker.seed(i);
-    var user = new voter( {  
-         region:    "location"+(cnt) ,
-         name : faker.name.findName(),
-         title : faker.name.suffix(),
-         email : faker.internet.email(),
-         password : faker.internet.password(),
-         image : faker.image.imageUrl(),
-         zipcode : faker.address.zipCode(),
-         city : faker.address.city(),
-         state: faker.address.state(),
-         streetaddress : faker.address.streetAddress(),
-         voted: 0,
-         votedTo: null,
+for(var i=0;i<10;i++){
+    // var cnt = i%100;
+    // if(i==0){ cntx = 100;}
+    // faker.seed(i);
+    var party = new party( {  
+         name : party[i],
+         abb : abbParties[i],
+
          identity : makeid()
 })
 
-        console.log(user)
-    user.save(function (err) {
+        console.log(party)
+    party.save(function (err) {
       if (err) return handleError(err);
       // saved!
       console.log("error in seeding voter")
