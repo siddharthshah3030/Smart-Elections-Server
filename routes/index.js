e7b9a6f246f55a0949dca687b5be4ee4e85013fd
siddharth shah <siddharthshah3030@gmail.com>
2019-01-16 17:54:21 +0530

    adding files for UI tenmpatre all publich static files in files folder

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('dashboard', { title: 'dashboard' });
 });
 router.get('/home', function(req, res, next) {
-  res.render('dashboard', { title: 'dashboard' });
+  res.render('index', { title: 'Express' });
 });
 
 
