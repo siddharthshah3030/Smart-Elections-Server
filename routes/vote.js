8d1dba8301f375ca3a7092af60284435c2e543dc
siddharth shah <siddharthshah3030@gmail.com>
2019-01-19 03:47:58 +0530

    message for voting session

--- routes/vote.js
+++ routes/vote.js
@@ -1,29 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.route('/up')
-  .post(
-    setConnectionTimeout('12h'),
-    require('./actions/upload-files').responseHandler
-  );
-
-function setConnectionTimeout(time) {
-  var delay = typeof time === 'string'
-    ? ms(time)
-    : Number(time || 5000);
-
-  return function (req, res, next) {
-    res.connection.setTimeout(delay);
-    next();
-  }
-}
+var session = 0;
 
 
 
 
 router.get('/', function(req, res, next) {
-    res.send('respond with a voting data');
+    if(session==0){
+        res.send('0');
+    }
+    else{
+        res.send('1');
+
+    }
 });
 
 
