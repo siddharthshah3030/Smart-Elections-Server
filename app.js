1078d4ccd52e048e4cd6bf0d2418e1c8090c3f36
siddharth shah <siddharthshah3030@gmail.com>
2019-01-19 02:51:59 +0530

    voting api url set

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/region', region);
+app.use('/vote', votingMachine);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/vote.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a voting data');
+});
+
+
+module.exports = router;
+
