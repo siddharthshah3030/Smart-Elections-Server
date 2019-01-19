37563767cdcf8c22767f3c8e5411075c876e0edd
siddharth shah <siddharthshah3030@gmail.com>
2019-01-19 11:39:21 +0530

    /vote post rquest

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var region = require('./routes/region');
 var votingMachine = require('./routes/vote');
+var buttonPressed = require('./routes/buttonPressed');
 
 
 //SiD
@@ -50,7 +51,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/region', region);
-app.use('/vote', votingMachine);
+app.use('/start', votingMachine);
+app.use('/vote', buttonPressed);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/buttonPressed.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+var session = 1;
+// var user_name ;
+var userid = "5c4244d5b1a3cc0e3c904cec";
+
+router.post('/',function(req,res){
+    var tempBody = {
+        userid : "5c4244d5b1a3cc0e3c904cec"
+
+    }
+    console.log(req.body);
+    // var user_name=req.body.user;
+     user_name=req.tempBody.userid;
+    // var password=req.body.password;
+    console.log("User name = "+user_name+", password is ");
+    session = 1;
+    setTimeout(function() {
+        session = 0;
+
+        console.log("time out function ")
+        //your code to be executed after 1 second
+      }, 10000);
+    // console.log("User name = "+user_name+", password is "+password);
+    res.end("yes");
+  });
+
+
+router.get('/', function(req, res, next) {
+    if(session==0){
+        res.send('0');
+    }
+    else{
+        // console.log("sending voter data before")
+        var userid = "5c4244d5b1a3cc0e3c904ce8";
+
+        voter.findById(userid, function (err, voter) {
+            campaign.findOne({ name: voter.region }, function (err, location) {
+                var str = "";
+                var int = 1;
+                location.candidates.map(e=>{
+
+                    console.log(e.party.abb)
+                    str = str + int+"-"
+                    str = str + e.party.abb
+                    str = str + " "
+
+                    int++;
+                })
+            console.log(str )
+
+            res.send(str);
+        });
+            // console.log(voter.region)
+            // console.log("iuchniu")
+        });
+        // voter.findOne({ id: 'ERMOxqvujy' }, function (err, adventure) {
+        //     console.log("sending voter data find one ")
+
+        // });
+
+
+
+    }
+});
+
+
+module.exports = router;
+
