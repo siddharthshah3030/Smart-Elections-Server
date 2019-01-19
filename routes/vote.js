716ea53dd8813daa66e585a1c27269724ab1a459
siddharth shah <siddharthshah3030@gmail.com>
2019-01-19 10:03:34 +0530

    addign vote session with time 10s

--- routes/vote.js
+++ routes/vote.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var session = 0;
+var user_name;
 
+app.post('/',function(req,res){
+    var tempBody = {
+        userid : "12345"
 
+    }
+    // var user_name=req.body.user;
+     user_name=req.tempBody.userid;
+    // var password=req.body.password;
+    console.log("User name = "+user_name+", password is ");
+    session = 1;
+    setTimeout(function() {
+        session = 0;
+
+        console.log("time out function ")
+        //your code to be executed after 1 second
+      }, 10000);
+    // console.log("User name = "+user_name+", password is "+password);
+    res.end("yes");
+  });
 
 
 router.get('/', function(req, res, next) {
